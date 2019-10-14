19a26e47f9e5256f57bf17ae51607b82ffde7b52
Harriet Alexander <harriet.xander@gmail.com>
2019-10-14 16:51:08 -0400

    added submit scripts

---
+++ submit_script/verb_dry_submit_snakemake.sh
@@ -0,0 +1,3 @@
+snakemake   \
+        --jobs 40 --use-conda --profile tara-metag  \
+        --cluster-config cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=TARA.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}" -np --verbose --reason
