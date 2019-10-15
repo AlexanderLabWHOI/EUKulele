1553d43e960603a26b9287fdf88817dc2f92aa2a
Harriet Alexander <harriet.xander@gmail.com>
2019-10-15 16:17:58 -0400

    added argparse

--- scripts/tax-placment.py
+++ scripts/tax-placment.py
@@ -3,6 +3,9 @@ import ujson
 import pandas as pd
 import yaml
 import chardet
+import argparse
+
+
 
 def tax_placement(pident):
     if pident >= tax_cutoffs['species']:
@@ -20,26 +23,26 @@ def tax_placement(pident):
 def read_in_taxonomy(infile):
     with open(infile, 'rb') as f:
         result = chardet.detect(f.read())
-    tax_table = pd.read_csv(tax_file, sep='\t',encoding=result['encoding']) 
-    tax_table.columns = tax_table.columns.str.lower() 
-    tax_table=tax_table.set_index('source_id')
-    return tax_table
+    tax_out = pd.read_csv(infile, sep='\t',encoding=result['encoding']) 
+    tax_out.columns = tax_out.columns.str.lower() 
+    tax_out =tax_out.set_index('source_id')
+    return tax_out
 
 def read_in_tax_cutoffs(yamlfile):
     with open(yamlfile, 'r') as stream:
-        tax_cutoffs = yaml.safe_load(stream)
-    return tax_cutoffs 
+        co_out = yaml.safe_load(stream)
+    return co_out
 
 def read_in_protein_map(protjson):
     with open(protjson, 'rb') as f:
-        pdict = ujson.load(f)
-    return pdict
+        pout = ujson.load(f)
+    return pout
 
 def read_in_diamond_file(dfile, pdict):
-    df =  pd.read_csv(dfile, sep = '\t', header = None)    
-    df.columns = ['qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']
-    df['ssqid_TAXID']=df.sseqid.map(pdict)
-    return df
+    dfout =  pd.read_csv(dfile, sep = '\t', header = None)    
+    dfout.columns = ['qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']
+    dfout['ssqid_TAXID']=dfout.sseqid.map(pdict)
+    return dfout
 
 def classify_taxonomy(df, tax_table):
     outdf = pd.DataFrame(columns = ['classification_level', 'classification', 'max_pid'])
@@ -62,14 +65,21 @@ def classify_taxonomy(df, tax_table):
     return outdf
 
 if __name__ == "__main__":
-    tax_file = '/vortexfs1/omics/alexander/data/mmetsp/reference_dir/taxonomy-table.txt'
-    tax_table = read_in_taxonomy(tax_file)
-    cutoff_file = "tax-cutoffs.yaml" 
-    tax_cutoffs = read_in_tax_cutoffs(cutoff_file)
-    prot_map_file = '/vortexfs1/omics/alexander/data/mmetsp/reference_dir/protein-species-map.json'
-    pdict = read_in_protein_map(prot_map_file)
-    diamond_file = 'output/METs/diamond/test2.diamond.out'
-    diamond_df = read_in_diamond_file(diamond_file, pdict)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tax_file')
+    parser.add_argument('--cutoff_file')
+    parser.add_argument('--prot_map_file')
+    parser.add_argument('--diamond_file')
+    parser.add_argument('--outfile')
+    args = parser.parse_args()
+    #tax_file = '/vortexfs1/omics/alexander/data/mmetsp/reference_dir/taxonomy-table.txt'
+    tax_table = read_in_taxonomy(args.tax_file)
+    #cutoff_file = "tax-cutoffs.yaml" 
+    tax_cutoffs = read_in_tax_cutoffs(args.cutoff_file)
+    #prot_map_file = '/vortexfs1/omics/alexander/data/mmetsp/reference_dir/protein-species-map.json'
+    pdict = read_in_protein_map(args.prot_map_file)
+    #diamond_file = 'output/METs/diamond/test2.diamond.out'
+    diamond_df = read_in_diamond_file(args.diamond_file, pdict)
     classification_df = classify_taxonomy(diamond_df, tax_table)
-    outfile = 'tmp.out'
-    classification_df.to_csv(outfile, sep='\t')
+    #outfile = 'tmp.out'
+    classification_df.to_csv(args.outfile, sep='\t')
