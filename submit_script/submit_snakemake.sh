6531adcf87c38c4faf032920591904137bfb8bde
Harriet Alexander <harriet.xander@gmail.com>
2020-02-16 23:02:52 -0500

    added tax id ipynb

--- submit_script/submit_snakemake.sh
+++
@@ -1,4 +0,0 @@
-snakemake   \
-        --jobs 100 --use-conda --profile tara-metag  \
-        --cluster-config big-bwa-cluster.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=TARA.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
-
