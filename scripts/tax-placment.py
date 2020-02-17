e3168285120330da614002ede931730228e5c9b6
Harriet Alexander <harriet.xander@gmail.com>
2020-02-16 23:02:11 -0500

    updated tax placement to include full phylogeny;

--- scripts/tax-placment.py
+++ scripts/tax-placment.py
@@ -23,8 +23,8 @@ def tax_placement(pident):
 def read_in_taxonomy(infile):
     with open(infile, 'rb') as f:
         result = chardet.detect(f.read())
-    tax_out = pd.read_csv(infile, sep='\t',encoding=result['encoding']) 
-    tax_out.columns = tax_out.columns.str.lower() 
+    tax_out = pd.read_csv(infile, sep='\t',encoding=result['encoding'])
+    tax_out.columns = tax_out.columns.str.lower()
     tax_out =tax_out.set_index('source_id')
     return tax_out
 
@@ -39,29 +39,43 @@ def read_in_protein_map(protjson):
     return pout
 
 def read_in_diamond_file(dfile, pdict):
-    dfout =  pd.read_csv(dfile, sep = '\t', header = None)    
+    dfout =  pd.read_csv(dfile, sep = '\t', header = None)
     dfout.columns = ['qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']
     dfout['ssqid_TAXID']=dfout.sseqid.map(pdict)
     return dfout
 
+
+
 def classify_taxonomy(df, tax_table):
-    outdf = pd.DataFrame(columns = ['classification_level', 'classification', 'max_pid'])
+    level_dict = {'class':['supergroup','division','class'],
+                       'order':['supergroup','division','class', 'order'],
+                       'family':['supergroup','division','class', 'order', 'family'],
+                       'genus': ['supergroup','division','class', 'order', 'family','genus'],
+                        'species':['supergroup','division','class', 'order', 'family','genus', 'species']}
+
+    outdf = pd.DataFrame(columns = ['classification_level', 'full_classification','classification', 'max_pid'])
     for t,dd in df.groupby('qseqid'):
         md = dd.pident.max()
         ds = list(set(dd[dd.pident==md]['ssqid_TAXID']))
         assignment = tax_placement(md)
+        full_assignment = level_dict[assignment]
         if len(ds)==1:
             classification = tax_table.loc[ds[0], assignment]
+            full_classification = tax_table.loc[ds[0], full_assignment]
+            full_classification='; '.join(list(full_classification))
         else:
             classification_0 = []
+            full_classification_0=[]
             for d in ds:
                 classification_0.append(tax_table.loc[ds[0], assignment])
+                fc = tax_table.loc[ds[0], full_assignment]
+                full_classification_0.append('; '.join(list(fc)))
                 if len(set(classification_0)) ==1:
                     classification = list(set(classification_0))[0]
+                    full_classification=list(set(full_classification_0))[0]
                 else:
-                    pass 
-        outdf.loc[t] = [assignment, classification, md]
-        
+                    pass
+        outdf.loc[t] =  [assignment, full_classification, classification, md]
     return outdf
 
 if __name__ == "__main__":
@@ -74,7 +88,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     #tax_file = '/vortexfs1/omics/alexander/data/mmetsp/reference_dir/taxonomy-table.txt'
     tax_table = read_in_taxonomy(args.tax_file)
-    #cutoff_file = "tax-cutoffs.yaml" 
+    #cutoff_file = "tax-cutoffs.yaml"
     tax_cutoffs = read_in_tax_cutoffs(args.cutoff_file)
     #prot_map_file = '/vortexfs1/omics/alexander/data/mmetsp/reference_dir/protein-species-map.json'
     pdict = read_in_protein_map(args.prot_map_file)
