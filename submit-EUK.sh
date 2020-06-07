d7b30be7ff8d8aa9ec073ab968e2a43f66b5a4d4
akrinos <akrinos@vt.edu>
2020-06-07 09:14:48 -0400

    Small tweaks to directories and file naming; blast option now functions properly

--- submit-EUK.sh
+++ submit-EUK.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH -N 1
+#SBATCH -p compute
+
+jobname=$(cat config.yaml | grep jobname | cut -d ":" -f 2 | cut -d " " -f 2)
+
 snakemake   \
         --jobs 100 --use-conda -s EUKulele \
-        --cluster-config EUKulele.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=TARA.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
+        --cluster-config EUKulele.yaml --cluster "sbatch --parsable --qos=unlim --partition={cluster.queue} --job-name=$jobname.{rule}.{wildcards} --mem={cluster.mem}gb --time={cluster.time} --ntasks={cluster.threads} --nodes={cluster.nodes}"
 
